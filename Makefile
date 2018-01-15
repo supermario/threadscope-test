8aea900cff7edb1303af93fcb14a6fa87e19f06c
Mario Rogic <hello@mario.net.au>
2018-01-15 16:50:36 +0000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+build:
+	@echo "Nope!"
+
+install:
+	# Requires adding to stack.yml
+	# extra-deps:
+	# - hp2pretty-0.8.0.2
+	# - floatshow-0.2.4
+	stack install hp2pretty
---
+++ app/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Lib
+
+import Control.Parallel.Strategies
+import System.Environment
+
+
+
+fib 0 = 1
+fib 1 = 1
+fib n = runEval $ do
+   x <- rpar (fib (n-1))
+   y <- rseq (fib (n-2))
+   return (x + y + 1)
+
+main = do
+   args <- getArgs
+   n <- case args of
+         []    -> return 20
+         [x]   -> return (read x)
+         _     -> fail ("Usage: hellofib [n]")
+   print (fib n)
